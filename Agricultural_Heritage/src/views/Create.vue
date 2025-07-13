11892fd41bac492b7d4d3b466089baa549cb45dd
siyue222 <3304509394@qq.com>
2025-07-13 15:27:36 +0800

    增加农遗智创板块

---
+++ Agricultural_Heritage/src/views/Create.vue
@@ -0,0 +1,54 @@
+import * as images from '@/assets/image.js'
+import { ref } from 'vue'
+
+// 数据定义保持不变
+const searchQuery = ref('')
+
+// 新生·探元轮播图数据
+const newExplorationItems = ref([
+  { image: images.ty1 },
+  { image: images.ty2 },
+  { image: images.ty3 },
+  { image: images.ty4 },
+  { image: images.ty5 },
+  { image: images.ty6 },
+  { image: images.ty7 }
+])
+
+// 新生·主题数据
+const featuredItems = ref([
+  { title: '乾隆面', image: images.food1 },
+  { title: '桑叶面', image: images.food2 },
+  { title: '印象一', image: images.yx1 },
+  { title: '印象二', image: images.yx2 },
+  { title: '渔塘一', image: images.yt1 },
+  { title: '渔塘二', image: images.yt2 },
+  { title: '图标一', image: images.tb1 },
+  { title: '图标二', image: images.tb2 }
+])
+
+// 线稿设色数据
+const lineArtItems = ref([
+  { title: '鱼戏荷花', image: images.xg1 },
+  { title: '荷塘月色', image: images.xg2 },
+  { title: '渔舟唱晚', image: images.xg3 },
+  { title: '稻花香里说丰年', image: images.xg4 },
+  { title: '农耕文化', image: images.xg5 },
+  { title: '乡村振兴', image: images.xg6 }
+])
+
+// 平面设计数据
+const designItems = ref([
+  { title: '店铺设计', image: images.pm1 },
+  { title: '产品包装', image: images.pm2 },
+  { title: '宣传海报', image: images.pm3 },
+  { title: '品牌形象', image: images.pm4 },
+  { title: '文化衍生品', image: images.pm5 },
+  { title: '视觉识别系统', image: images.pm6 }
+])
+
+// 标签数据
+const tags = ref([
+  '全部', '农遗美食', '传统工艺', '文化景点', '非遗传承', '手工艺品'
+])
+</script>
--- src/assets/image.js
+++ src/assets/image.js
@@ -32,8 +32,41 @@ import startBtn from './组件 10 – 1.png'
 //数字基因图谱
 import digitalGeneMap from './数字基因.png'
 
+//智创
+import nongyi from './农遗新生.png' 
 
+//新生 探元
+import ty1 from './ty1.png'
+import ty2 from './ty2.png' 
+import ty3 from './ty3.png'
+import ty4 from './ty4.png'
+import ty5 from './ty5.png'
+import ty6 from './ty6.png'
+import ty7 from './ty7.png'
 
+//新生 主题
+import food1 from './food1.png'
+import food2 from './food2.png'
+import yx1 from './印象1.png'
+import yx2 from './印象2.png'
+import yt1 from './鱼塘1.png'
+import yt2 from './鱼塘2.png' 
+import tb1 from './图标1.png'
+import tb2 from './图标2.png' 
+
+import xg1 from './线稿1.png'
+import xg2 from './线稿2.png'
+import xg3 from './线稿3.png'
+import xg4 from './线稿4.png'
+import xg5 from './线稿5.png'
+import xg6 from './线稿6.png'
+
+import pm1 from './平面1.png'
+import pm2 from './平面2.png'
+import pm3 from './平面3.png'
+import pm4 from './平面4.png'
+import pm5 from './平面5.png'
+import pm6 from './平面6.png'
 
 export {
   banner1, banner2, banner3,
@@ -42,5 +75,10 @@ export {
   huzhou, shaoxing, anxi, minhou, nantian, more,
   locationIcon, locationText,
   mainLogo, titleImg, startBtn,
-  digitalGeneMap
+  digitalGeneMap,
+  nongyi,
+  ty1, ty2, ty3, ty4, ty5, ty6, ty7,
+  food1, food2,yx1, yx2,yt1, yt2,tb1, tb2,
+  xg1, xg2, xg3, xg4, xg5, xg6,
+  pm1, pm2, pm3, pm4, pm5, pm6
 }
