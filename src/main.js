0254d83c6239ac2724fbef56ac43e678eef2d6a3
VISIER <3146088404@qq.com>
2025-09-08 19:58:14 +0800

    revise the dy im

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // 引入路由配置
 // 按需导入Element Plus
-import { ElCarousel, ElCarouselItem, ElRow, ElCol, ElCard } from 'element-plus'
+import { ElCarousel, ElCarouselItem, ElRow, ElCol, ElCard, ElMenu, ElMenuItem } from 'element-plus'
 import 'element-plus/es/components/carousel/style/css'
 import 'element-plus/es/components/carousel-item/style/css'
 import 'element-plus/es/components/row/style/css'
 import 'element-plus/es/components/col/style/css'
 import 'element-plus/es/components/card/style/css'
+import 'element-plus/es/components/menu/style/css'
+import 'element-plus/es/components/menu-item/style/css'
 
 const app = createApp(App)
 
@@ -17,6 +19,8 @@ app.component(ElCarouselItem.name, ElCarouselItem)
 app.component(ElRow.name, ElRow)
 app.component(ElCol.name, ElCol)
 app.component(ElCard.name, ElCard)
+app.component(ElMenu.name, ElMenu)
+app.component(ElMenuItem.name, ElMenuItem)
 
 app.use(router)
 app.mount('#app')
